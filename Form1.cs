caed900af15fbb96c6fe03088a5c6572e7328d99
Naveenaik <naveenaik5092003@gmail.com>
2024-10-13 12:47:16 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NumberGuessing
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int min = 1;
+        int max = 100;
+        int number;
+        int guesses;
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            number = random.Next(min, max + 1);
+            guesses = 0;
+            lblResult.Text = "";
+            txtGuess.Text = "";
+            lblMessage.Text = $"Enter a number between {min} - {max}:";
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int guess;
+
+            if (int.TryParse(txtGuess.Text, out guess))
+            {
+                guesses++;
+                if (guess > number)
+                {
+                    lblResult.Text = $"{guess} is too high!!!";
+                }
+                else if (guess < number)
+                {
+                    lblResult.Text = $"{guess} is too low!!!";
+                }
+                else
+                {
+                    lblResult.Text = $"Number: {number}\nYOU WIN!!!...\nGuesses: {guesses}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+                txtGuess.Clear(); // Clear the textbox for a new entry
+                txtGuess.Focus();  // Set focus back to the textbox
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
---
+++ NumberGuessing/CodeFile1.cs
@@ -0,0 +1,96 @@
+﻿namespace NumberGuessing
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(12, 9);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(200, 13);
+            this.lblMessage.TabIndex = 0;
+
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(15, 35);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 1;
+
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(15, 61);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 100);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 3;
+
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(15, 120);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 4;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Form1";
+            this.Text = "Number Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
